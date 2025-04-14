5ed3f9c401076f0a1f5dd8bcf52d219cd821e7a8
Ruba Al-Hilal <developerruba@outlook.com>
2025-04-14 19:57:44 +0300

    add files

---
+++ src/components/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Product([products]) {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ProductList() {
+    const [products, setProducts]= useState([]);
+    const fetchData = ()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+    use
   return (
     <div>ProductList</div>
+    // <Product product = [products]/>
   )
 }
 
